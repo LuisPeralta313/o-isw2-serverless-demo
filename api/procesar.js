1e0e08366ea6a093f5745079bc7f0cfbf9d19b76
Luis Peralta <92604074+LuisPeralta313@users.noreply.github.com>
2026-02-12 21:57:27 -0600

    Refactor handler function and remove commented code

--- api/procesar.js
+++ api/procesar.js
@@ -1,31 +1,29 @@
-//Código original prueba 1
-//export default function handler(req, res) {
-  //const nombre = req.query.nombre || "anónimo";
-
-  //res.status(200).json({
-    //resultado: `Nombre procesado: ${nombre.toUpperCase()}`
-  //});
-//}
+export default function handler(req, res) {
+  const nombre = req.query.nombre || "anónimo";
+  res.status(200).json({
+    resultado: `Nombre procesado: ${nombre.toUpperCase()}`
+  });
+}
 
 // código Mini reto agregando timestamp y simulando error  si nombre= "error"
-module.exports = (req, res) => {
-  const { nombre = "Visitante" } = req.query;
+//module.exports = (req, res) => {
+  //const { nombre = "Visitante" } = req.query;
   
 
-  if (nombre.toLowerCase() === "error") {
-    return res.status(500).json({
-      error: "Error simulado",
-      mensaje: "El procesamiento falló intencionalmente",
-      timestamp: new Date().toISOString()
-    });
-  }
+  //if (nombre.toLowerCase() === "error") {
+    //return res.status(500).json({
+      //error: "Error simulado",
+      //mensaje: "El procesamiento falló intencionalmente",
+      //timestamp: new Date().toISOString()
+    //});
+  //}
   
-  const timestamp = new Date().toISOString();
+  //const timestamp = new Date().toISOString();
   
-  res.status(200).json({
-    mensaje: `Hola ${nombre.toUpperCase()}, tu solicitud ha sido procesada`,
-    procesadoPor: "/api/procesar",
-    timestamp: timestamp,
-    longitudNombre: nombre.length
-  });
-};
+  //res.status(200).json({
+    //mensaje: `Hola ${nombre.toUpperCase()}, tu solicitud ha sido procesada`,
+    //procesadoPor: "/api/procesar",
+    //timestamp: timestamp,
+    //longitudNombre: nombre.length
+  //});
+//};
